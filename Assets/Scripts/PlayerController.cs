5f50bb3d871c26f4b13789937092e0e0dcf7938c
YutoImata <yuto.imata.wine@gamil.com>
2025-03-28 14:38:43 +0900

    ジャンプの処理を追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5.0f;
+    public float jumpPower = 5.0f;
     private Rigidbody rb;
+
+    /* フラグの設定 */
+    private bool isGrounded = true;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,5 +28,23 @@ public class PlayerController : MonoBehaviour
         Vector2 moveInput = context.ReadValue<Vector2>();
 
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+
     }
 }
