ac73c9610eef877994230bd527d7b645bad06ba1
YutoImata <yuto.imata.wine@gamil.com>
2025-04-02 01:56:10 +0900

    画像を並べるスクリプトを作成

---
+++ Assets/Scripts/Other/AlignSprites.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignSprites : MonoBehaviour
+{
+    [SerializeField] private float currentX = 0.0f; /* 現在のX座標 */
+
+    public GameObject[] images; /* 並べたい画像のオブジェクトを入れるよう */
+    public float spacing = 0.0f; /* 画像の間隔 */
+
+    void Start()
+    {
+        AlignImages();
+    }
+
+    private void AlignImages()
+    {
+        foreach (GameObject image in images)
+        {
+            if(image.TryGetComponent<SpriteRenderer>(out SpriteRenderer sr))
+            {
+                float width = sr.bounds.size.x; /* 画像の幅を取得 */
+                image.transform.position = new Vector3(currentX, 0, 0); /* 横位置を設定 */
+                currentX += width + spacing; /* 次の画像のX座標を計算 */
+            }
+        }
+    }
+}
