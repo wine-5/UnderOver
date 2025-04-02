0702d6746029b5ecc417a3a87a5f449aef4f0f7b
YutoImata <yuto.imata.wine@gamil.com>
2025-04-02 15:45:31 +0900

    PlayerのHPのIconを追加

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,34 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     /* プレイヤーのHP */
     [SerializeField] private int currentHP = 3;
+    public List<Image> hpIcons; /* HPアイコンを入れる変数 */
 
     /* Playerを入れる変数 */
     public GameObject player;
+
+
     void Start()
     {
         Debug.Log("今のPlayerのHP" + currentHP);
-    }
-
-    void Update()
-    {
-        
+        currentHP = hpIcons.Count; /* HPアイコンの数を取得 */
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            currentHP--;
-            Debug.Log("攻撃を受けた後のPlayerのHP" + currentHP);
-            if(currentHP <= 0)
-            {
-                player.SetActive(false);
-            }
+            TakeDamage(); /* 攻撃を受けたときの処理 */
+        }
+    }
+
+    private void TakeDamage()
+    {
+        currentHP--;
+        Debug.Log("攻撃を受けた後のPlayerのHP" + currentHP);
+        hpIcons[currentHP].enabled = false; /* HPアイコンを非表示にする */
+        if (currentHP <= 0)
+        {
+            player.SetActive(false);
         }
     }
 }
