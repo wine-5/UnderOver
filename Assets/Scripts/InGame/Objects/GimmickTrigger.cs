d5adf9387f6187d4de75c0d33d91ff45f46c7c51
YutoImata <yuto.imata.wine@gamil.com>
2025-04-21 10:18:06 +0900

    とげの動きを往復か一方向かを選択できるようにし、Stage3の設計が完了した

---
+++ Assets/Scripts/InGame/Objects/GimmickTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickTrigger : MonoBehaviour
+{
+    [Header("とげの親オブジェクト")]
+    [SerializeField] private GameObject spikesParent;
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(hasTriggered) return;
+
+        if(other.CompareTag("Player"))
+        {
+            if(spikesParent != null)
+            {
+                spikesParent.SetActive(true);
+                hasTriggered = true;
+            }
+            else
+            {
+                Debug.LogError("spikesParentがアタッチされていない");
+            }
+        }
+    }
+}
--- Assets/Scripts/InGame/Objects/MovingSpikes.cs
+++ Assets/Scripts/InGame/Objects/MovingSpikes.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class MovingSpikes : MonoBehaviour
 {
-    /* 縦方向か横方向 */
+    /* 動く方向の設定 */
     public enum Direction
     {
         UpDown,
         LeftRight,
     }
-
+    /* 動く方向の設定 */
+    public enum MoveMode
+    {
+        PingPong, /* 往復 */
+        OneWay /* 一方向 */
+    }
     /* とげの基本設定 */
     [Header("とげの基本設定")]
     [SerializeField] private Direction moveDirection = Direction.UpDown;
+    [SerializeField] private MoveMode moveMode = MoveMode.PingPong; /* 初期値は往復 */
     [SerializeField] private float moveDistance = 2f;
     [SerializeField] float moveSpeed = 2f;
     private Vector3 startPos;
+    private float moveTimer = 0f;
     void Start()
     {
         startPos = transform.position;
@@ -24,7 +31,18 @@ public class MovingSpikes : MonoBehaviour
 
     void Update()
     {
-        float offset = Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+        moveTimer += Time.deltaTime;
+        float offset = 0;
+
+        if(moveMode == MoveMode.PingPong)
+        {
+            offset = Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+        }
+        else if(moveMode == MoveMode.OneWay)
+        {
+            offset = -moveTimer * moveSpeed; /* 一方向に進む（左）*/
+            offset = Mathf.Max(offset, -moveDistance);
+        }
 
         if(moveDirection == Direction.UpDown)
         {
