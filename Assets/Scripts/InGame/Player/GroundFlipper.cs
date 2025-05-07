5ba3e0da361d6d9a1c4c4d8313619607478a0b6b
YutoImata <yuto.imata.wine@gmail.com>
2025-05-07 17:01:38 +0900

    とげにめり込むバグを修正

--- Assets/Scripts/InGame/Player/GroundFlipper.cs
+++ Assets/Scripts/InGame/Player/GroundFlipper.cs
@@ -22,6 +22,9 @@ public class GroundFlipper : MonoBehaviour
     private const float INVERT_ROTATION = 180.0f;
 
 
+    /// <summary>
+    /// 初期化処理をしている処理
+    /// </summary>
     void Start()
     {
         if (playerObject != null)
@@ -33,10 +36,11 @@ public class GroundFlipper : MonoBehaviour
         {
             Debug.LogError("PlayerObjectが設定されていない");
         }
-
-
     }
 
+    /// <summary>
+    /// 左クリックをしているかを監視している
+    /// </summary>
     void Update()
     {
         // if (isGroundFlip)
@@ -59,6 +63,9 @@ public class GroundFlipper : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Playerが反転したときの処理
+    /// </summary>
     private void FlipGround()
     {
         isGroundFlip = true; /* 地面が反転している */
@@ -71,9 +78,10 @@ public class GroundFlipper : MonoBehaviour
 
         if (hit.collider != null) /* Raycastが地面に当たった場合 */
         {
+            float offset = GetPlayerColliderHeight() / 2f;
             Vector2 newPosition = hit.point; /* Raycastが当たった地面の位置を取得 */
             // Debug.Log(hit.collider.gameObject.name); /* デバッグ用に地面の位置を表示 */
-            newPosition.y -= 0.1f; /* めり込みするのを防止するために少し下に設定 */
+            newPosition.y -= offset; /* めり込みするのを防止するために少し下に設定 */
 
             playerTransform.position = newPosition; /* Playerの位置を地面の位置に設定 */
         }
@@ -85,6 +93,9 @@ public class GroundFlipper : MonoBehaviour
         rb.gravityScale = -1; /* 重力を逆にする */
     }
 
+    /// <summary>
+    /// Playerが元の地面に戻った際の処理
+    /// </summary>
     private void ResetGround()
     {
         isGroundFlip = false;
@@ -95,9 +106,10 @@ public class GroundFlipper : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(playerTransform.position, Vector2.down, Mathf.Infinity, groundLayer);
         if (hit.collider != null) /* Raycastが地面に当たった場合 */
         {
+            float offset = GetPlayerColliderHeight() / 2f;
             Vector2 newPosition = hit.point; /* Raycastが当たった地面の位置を取得 */
             // Debug.Log(hit.collider.gameObject.name); /* デバッグ用に地面の位置を表示 */
-            newPosition.y -= -0.1f; /* めり込みするのを防止するために少し下に設定 */
+            newPosition.y += offset; /* めり込みするのを防止するために少し下に設定 */
 
             playerTransform.position = newPosition; /* Playerの位置を地面の位置に設定 */
         }
@@ -105,5 +117,19 @@ public class GroundFlipper : MonoBehaviour
         AudioManager.Instance.PlaySE("seGroundFlip"); /* SEを再生 */
 
         rb.gravityScale = 1;
+    }
+
+    /// <summary>
+    /// とげにめり込まないためにPlayerの高さを調節する
+    /// </summary>
+    /// <returns></returns>
+    private float GetPlayerColliderHeight()
+    {
+        Collider2D col = playerObject.GetComponent<Collider2D>();
+        if (col != null)
+        {
+            return col.bounds.size.y;
+        }
+        return 1.0f;
     }
 }
